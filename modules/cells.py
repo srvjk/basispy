30e03c75dd2e454565997dd3adf2d83ff473fb97
Serge Gron <serge.gron@gmail.com>
2021-06-28 23:39:54 +0300

    --

--- modules/cells.py
+++ modules/cells.py
@@ -1,4 +1,6 @@
 import basis
+import pygame
+import sys
 
 
 class Board(basis.Entity):
@@ -7,10 +9,28 @@ class Board(basis.Entity):
 
 
 class Viewer(basis.Entity):
+    def __init__(self):
+        pygame.init()
+        size = width, height = 640, 480
+        self.screen = pygame.display.set_mode(size)
+
     def step(self):
-        print("-> viewer")
+        #print("-> viewer")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
-class Agent:
+class Agent(basis.Entity):
+    def __init__(self):
+        self._position = [0, 0]
+
+    @property
+    def position(self):
+        return self._position
+
     def step(self):
         pass
